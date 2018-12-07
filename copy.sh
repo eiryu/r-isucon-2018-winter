87329efdd6a4fc6ccd4c5e0deeea3b0924ccc067
eiryu <eiryu@r.recruit.co.jp>
2018-12-07 14:48:59 +0000

    [modify] mysql を r2 に向ける

--- copy.sh
+++ copy.sh
@@ -2,5 +2,5 @@
 set -xe
 sudo cp mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo cp nginx.conf /etc/nginx/nginx.conf
-sudo cp /var/lib/mysql/mysqld-slow.log ~
-sudo cp /var/log/nginx/access_log.ltsv ~
+#sudo cp /var/lib/mysql/mysqld-slow.log ~
+#sudo cp /var/log/nginx/access_log.ltsv ~
