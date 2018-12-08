e81f51b2187ee653f76f0afcded4cc5039572a8c
eiryu <eiryu@r.recruit.co.jp>
2018-12-08 11:38:32 +0900

    ロギング外す

--- java/src/main/java/jp/co/recruit/rine/LoggingInterceptor.java
+++ java/src/main/java/jp/co/recruit/rine/LoggingInterceptor.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+//@Component
 public class LoggingInterceptor extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
